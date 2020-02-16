74d1f038605ce6a2a5703dfda63919aab933d526
Ray <kwanlokto@gmail.com>
2020-02-16 12:21:00 -0500

    create base classes for genetic algorithms

---
+++ genetic_algorithms/base_DNA.py
@@ -0,0 +1,73 @@
+from copy import deepcopy
+
+from genetic_algorithms.base_trait import BaseTrait
+
+
+class BaseDNA:
+    def __init__(self, traits: list, target):
+        self.__traits = traits
+        self.size = len(traits)
+        self.__check_traits()
+        self.target = target
+
+    def __repr__(self):
+        return f"DNA: {self.__traits}"
+
+    def __check_traits(self):
+        """
+        Raises an exception if the traits are not unique
+        """
+        if len(set([trait.get_id() for trait in self.__traits])) != self.size:
+            raise Exception("Traits are not unique")
+
+    def get_dna(self) -> list:
+        """
+        Get a copy of all traits for this DNA strand
+
+        Returns:
+            list: List of all traits
+        """
+        return deepcopy(self.__traits)
+
+    def get_trait(self, trait_id) -> BaseTrait:
+        """
+        Get a single trait from DNA by its id
+
+        Args:
+            trait_id: a unique identifier to determine the trait we want
+
+        Returns:
+            Trait: Trait with id trait_id
+        """
+        selected_trait = None
+        for trait in self.__traits:
+            selected_trait = trait if trait.get_id() == trait_id else selected_trait
+        return selected_trait
+
+    def mutate(self, mutation_rate: float):
+        """
+        Mutate the this DNA strand with some probability 'mutation_rate' 
+        for each cell
+
+        Args:
+            mutation_rate (float): Determines the probability of mutating the dna strand
+                                   (each trait)
+        """
+        pass
+
+    @property
+    def fitness(self) -> float:
+        """
+        Determines how well this DNA strand fits the target
+
+        Args:
+            target: The target we want to reach
+        """
+        pass
+
+    @classmethod
+    def cross_over(cls, parents: list):
+        """
+        Determines how to create child DNA from parent DNA
+        """
+        pass
---
+++ genetic_algorithms/base_trait.py
@@ -0,0 +1,74 @@
+from copy import deepcopy
+from random import choice, random
+
+
+class BaseTrait:
+    def __init__(self, trait_id, trait_pool: list = None):
+        self.__id = trait_id
+        self.__trait_pool = trait_pool
+        # every possible trait has equal probability to be chosen
+        self.randomly_set_value()
+
+    def __eq__(self, other):
+        return isinstance(other, BaseTrait) and other.__id == self.__id
+
+    def __repr__(self):
+        return f"{self.__id}: {self.value}"
+
+    def get_id(self):
+        """
+        Gets the id for the current Trait
+
+        Returns:
+            Object: ID for current Trait
+        """
+        return self.__id
+
+    def get_value(self):
+        """
+        Gets the value associated with this Trait
+
+        Returns:
+            Object: value for current Trait
+        """
+        return self.value
+
+    def set_value(self, new_value):
+        """
+        Update the value for the current Trait
+        """
+        self.value = new_value
+
+    def get_trait_pool(self) -> list:
+        """
+        Get all possible values that can be assigned to this trait
+
+        Returns:
+            List: The trait pool
+        """
+        return deepcopy(self.__trait_pool)
+
+    def randomly_set_value(self):
+        """
+        Randomly update this traits value
+        """
+        self.set_value(
+            choice(self.__trait_pool) if self.__trait_pool is not None
+            else random()
+        )
+
+    @classmethod
+    def combine_trait_pool(cls, first_trait, second_trait):
+        """
+        Combine the trait pool for the same trait but different pools
+        """
+        if first_trait.equal(second_trait):
+            new_trait_pool = [
+                *first_trait.get_trait_pool(),
+                *second_trait.get_trait_pool(),
+            ]
+
+            # TODO: Need to find some way to simplify it while maintaining ratios
+            return cls(first_trait.__id, new_trait_pool)
+
+        raise Exception("Cannot merge trait pools of two different traits")
