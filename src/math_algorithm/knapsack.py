4cfd87d5dc501b5cb3f9710a3b2b996d5fea2ad5
Ray <kwanlokto@gmail.com>
2020-05-22 17:49:20 -0400

    fix knapsack algorithm

--- src/math_algorithm/knapsack.py
+++ src/math_algorithm/knapsack.py
@@ -2,6 +2,7 @@ def regular_knapsack(bag_size, items):
     """
     Maximize total value when only taking 'bag_size' items from 'items'
 
+    OPT(i) = max()
     Args:
         bag_size (int): number of items which we can carry
         items (list): the value of each associated item
@@ -10,22 +11,30 @@ def regular_knapsack(bag_size, items):
     """
     bag_size += 1
     values = [
-        [] for _ in range(bag_size)
-    ]  # The best value after taking i items where i is index in the array
+        [[] for _ in range(bag_size)] for _ in range(len(items))
+    ]  # The best value after taking i items
 
-    for item_num in range(0, len(items)):
-        for items_taken in range(
-            bag_size - 1, 0, -1
-        ):  # Reverse order so we don't double add
-            total = sum_by_idx(items, values[items_taken])
-            new_total = (
-                sum_by_idx(items, values[items_taken - 1])
+    for item_num in range(len(items)):
+        for items_taken in range(1, bag_size):
+            total = sum_by_idx(items, values[item_num - 1][items_taken])
+
+            new_total = (  # assume weight 1
+                sum_by_idx(
+                    items, values[item_num - 1][items_taken - 1]
+                )
                 + items[item_num]
             )
-            if total < new_total:
-                values[items_taken] = values[items_taken - 1] + [item_num]
 
-    return values[-1]
+            # OPT[i][j] = max(OPT[i-1][j-1] + val[i], OPT[i-1][j])
+            if total < new_total:
+                values[item_num][items_taken] = values[item_num - 1][
+                    items_taken - 1
+                ] + [item_num]
+            else:
+                values[item_num][items_taken] = values[item_num - 1][
+                    items_taken
+                ]
+    return values[-1][-1]  # only bc weight is 1
 
 
 def sum_by_idx(array, indices):
--- src/math_algorithm/tests/test_knapsack.py
+++ src/math_algorithm/tests/test_knapsack.py
@@ -5,5 +5,5 @@ from src.math_algorithm.knapsack import regular_knapsack
 
 class TestKnapsack(unittest.TestCase):
     def test_regular_knapsack(self):
-        array = [90, 20, 30, 40, 50, 60, 70, 80, 90]
+        array = [90, 20, 30, 40, 50, 60, 70, 60, 80]
         self.assertEquals(regular_knapsack(2, array), [0, 8])
